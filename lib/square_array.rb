cb46ac086af985312070371065d74ebaeb8795e9
James Sapp <jameseisapp@gmail.com>
2020-12-02 20:12:14 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -6,7 +6,9 @@ def square_array(array)
     counter = 0 
     
     array.length.times do |index|
-      binding.pry 
+      new_array.push(array[index] ** 2)
+      counter += 1 
     end 
+    new_array
     
 end
